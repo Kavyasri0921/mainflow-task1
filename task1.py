96f57f3bde91f82c2026062041510f5383cb4430
Kavyasri0921 <kavyasai.0921@gmail.com>
2024-05-11 12:06:09 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,41 @@
-a=10
-b=20
-print(a+b)
+# Create a list
+my_list = [1, 2, 3, 4, 5]
+
+# Add an element to the list
+my_list.append(6)
+
+# Remove an element from the list
+my_list.remove(3)
+
+# Modify an element in the list
+my_list[0] = 10
+
+print("List:")
+print(my_list)
+
+# Create a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Add a key-value pair to the dictionary
+my_dict['d'] = 4
+
+# Remove a key-value pair from the dictionary
+del my_dict['b']
+
+# Modify a value in the dictionary
+my_dict['a'] = 100
+
+print("Dictionary:")
+print(my_dict)
+
+# Create a set
+my_set = {1, 2, 3, 4, 5}
+
+# Add an element to the set
+my_set.add(6)
+
+# Remove an element from the set
+my_set.remove(3)
+
+print("Set:") 
+print(my_set)
